32f12a369e2d493cb749dac8683a2207bb70b98f
Fahim H <fahimhussain21@gmail.com>
2024-10-19 16:24:19 -0400

    update projects and resume

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function About() {
   return (
     <div>
@@ -6,16 +8,19 @@ export default function About() {
         Hello, I'm <b>Fahim!</b> I enjoy messing around with technology and building solutions to problems through CS.
       </p>
       <p>
-        I will be attending{" "}
+        I'm a current first-year at {" "}
         <a href="https://nyu.edu" target="_blank">
           <b>NYU</b>
         </a>{" "}
-        in the fall where I'll be studying computer science.
+        studying computer science. You can see my previous experiences and education in detail on my {" "}
+      <Link target="_blank" href="/resume.pdf">
+        <b>resume</b>
+      </Link>{"."}
       </p>
       <p>
         If you'd like to get in touch or have any inquires you can reach out to me at{" "}
         <a href="mailto:fahimhussain21@gmail.com">
-          <b>fahimhussain21@gmail.com</b>
+          <b>fahimhussain21@gmail.com</b>{"."}
         </a>
       </p>
     </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,21 @@
-import { FaApple, FaBluetooth, FaMobileAlt, FaPython, FaReact } from "react-icons/fa";
-import { SiAndroid, SiArduino, SiClerk, SiPostgresql } from "react-icons/si";
-import { BiLogoFlask } from "react-icons/bi";
-import { RiJavaLine, RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
+import { FaApple, FaBluetooth, FaGithub, FaMobileAlt, FaReact } from "react-icons/fa";
+import { SiAndroid, SiArduino, SiClerk, SiGradle, SiPostgresql } from "react-icons/si";
+import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
 import { TbBrandMysql, TbBrandReactNative } from "react-icons/tb";
 import { MdLock } from "react-icons/md";
+import { PiBracketsCurly } from "react-icons/pi";
 
 const projects = [
   {
+    title: "AtomHacks Website",
+    description: "Front page for high school hackathon + fully custom dashboard for submissions that handled 150+ users during the event",
+    href: "https://atomhacks.org",
+    technologies: [<FaReact key={1} />, <RiNextjsFill key={2} />, <RiTailwindCssFill key={3} />, <TbBrandMysql key={4} />, <MdLock key={5} />],
+    alt: "React with Next.js, Tailwind CSS, MySQL, NextAuth",
+  },
+  {
     title: "SideQuest",
-    description: "An innovative gig platform built exclusively for broke college students",
+    description: "An innovative mobile app platform built for broke college students to do work for other students as a side hustle\n\nWon 2nd at All Star Code's Technical Entrepreneurship Incubator",
     href: "https://github.com/frykher/SideQuest",
     technologies: [<TbBrandReactNative key={1} />, <FaMobileAlt key={2} />, <FaApple key={3} />, <SiAndroid key={4} />, <SiPostgresql key={5} />, <SiClerk key={6} />],
     alt: "React Native, PostgreSQL, Clerk Auth",
@@ -16,23 +23,16 @@ const projects = [
   {
     title: "Bloomie",
     description:
-      "An artificial plant built with microelectronics that teaches people of all ages how to care for plants through a sensor based feedback loop. Built with a group at MIT's Beaverworks Summer Institute.",
+      "An artificial plant built with microelectronics that teaches people of all ages how to care for plants through a sensor based feedback loop\n\nBuilt with a group at MIT's Beaverworks Summer Institute.",
     href: "https://www.youtube.com/watch?v=luvmYKZcUog",
     technologies: [<SiArduino key={1} />, <TbBrandReactNative key={2} />, <FaMobileAlt key={3} />, <FaBluetooth key={4} />],
     alt: "Arduino Platform, React Native (optional mobile app), Bluetooth",
   },
   {
-    title: "AtomHacks Website",
-    description: "Front page for high school hackathon + fully custom dashboard for submissions",
-    href: "https://atomhacks.org",
-    technologies: [<FaReact key={1} />, <RiNextjsFill key={2} />, <RiTailwindCssFill key={3} />, <TbBrandMysql key={4} />, <MdLock key={5} />],
-    alt: "React with Next.js, Tailwind CSS, MySQL, NextAuth",
-  },
-  {
-    title: "Rona-Radar",
-    description: "An app to see COVID spread and locate hospitals in your area. Won third at HACKKU 21",
-    href: "https://github.com/Rona-Radar/app",
-    technologies: [<RiJavaLine key={1} />, <SiAndroid key={2} />, <FaPython key={3} />, <BiLogoFlask key={4} />],
+    title: "JetBrains Kanagawa Theme",
+    description: "A theme for JetBrains IDEs with 30k+ downloads and CI/CD pipelines",
+    href: "https://github.com/fahimmehraj/jetbrains-kanagawa-theme",
+    technologies: [<SiGradle key={1} />, <FaGithub key={2}/>, <PiBracketsCurly key={3} />],
     alt: "Java, Android, Python, Flask",
   },
 ];
@@ -46,7 +46,7 @@ export default function Projects() {
           <a title={project.alt} href={project.href} target="_blank" key={i}>
             <div className="flex flex-col bg-gray-200/5 h-full rounded-xl p-4 hover:cursor-pointer hover:shadow-[-4px_4px_0px_#52eaff] hover:translate-x-2 transition-all">
               <h2 className="mb-4">{project.title}</h2>
-              <p className="no-underline">{project.description}</p>
+              <p className="no-underline whitespace-pre-line">{project.description}</p>
               <span className="mt-auto text-slate-400 text-xl flex gap-2">{project.technologies}</span>
             </div>
           </a>
